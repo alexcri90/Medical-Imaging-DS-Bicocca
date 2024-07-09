fda5b0ff4383940b391844681019de78e75e0a95
matteo <matte.mondini@gmail.com>
2024-07-09 22:19:26 +0200

    feat: test set implemented

--- testing_segm.py
+++ testing_segm.py
@@ -40,6 +40,7 @@ from training_segm import run_length_encode, run_length_decode
 class TestDataset(Dataset):
     def __init__(self, root, df, size, mean, std, tta=4):
         self.root = root
+        self.df = df
         self.size = size
         self.fnames = list(df["ImageId"])
         self.num_samples = len(self.fnames)
@@ -54,7 +55,16 @@ class TestDataset(Dataset):
     def __getitem__(self, idx):
         fname = self.fnames[idx]
         path = os.path.join(self.root, fname + ".dcm")
-        image = cv2.imread(path)
+        print(path)
+        # image = cv2.imread(path)
+        dcm = pydicom.dcmread(path)
+        image = dcm.pixel_array
+        image = cv2.cvtColor(
+            image, cv2.COLOR_GRAY2RGB
+        )  # Assuming grayscale images, convert to RGB
+        image = cv2.resize(
+            image, (self.size, self.size)
+        )  # Resize to the same size as the mask
         images = self.transform(image=image)["image"]
         return images
 
@@ -75,47 +85,54 @@ def post_process(probability, threshold, min_size):
     return predictions, num
 
 
-size = 512
-mean = (0.485, 0.456, 0.406)
-std = (0.229, 0.224, 0.225)
-num_workers = 8
-batch_size = 16
-best_threshold = 0.5
-min_size = 3500
-# device = torch.device("cuda:0")
-device = torch.device("mps")
-# df = pd.read_csv("./results.csv")
-df = pd.DataFrame(columns=["ImageId", "EncodedPixels"])
-testset = DataLoader(
-    TestDataset("./train-rle-test-10.csv", df, size, mean, std),
-    batch_size=batch_size,
-    shuffle=False,
-    num_workers=num_workers,
-    pin_memory=True,
-)
-model = smp.Unet("resnet34", encoder_weights="imagenet", activation=None)
+if __name__ == "__main__":
 
-# model = model_trainer.net  # get the model from model_trainer object
-model.eval()
-state = torch.load("./model.pth", map_location=lambda storage, loc: storage)
-model.load_state_dict(state["state_dict"])
-encoded_pixels = []
-for i, batch in enumerate(tqdm(testset)):
-    preds = torch.sigmoid(model(batch.to(device)))
-    preds = (
-        preds.detach().cpu().numpy()[:, 0, :, :]
-    )  # (batch_size, 1, size, size) -> (batch_size, size, size)
-    for probability in preds:
-        if probability.shape != (1024, 1024):
-            probability = cv2.resize(
-                probability, dsize=(1024, 1024), interpolation=cv2.INTER_LINEAR
-            )
-        predict, num_predict = post_process(probability, best_threshold, min_size)
-        if num_predict == 0:
-            encoded_pixels.append("-1")
-        else:
-            r = run_length_encode(predict)
-            encoded_pixels.append(r)
-df["EncodedPixels"] = encoded_pixels
-df.to_csv("submission.csv", columns=["ImageId", "EncodedPixels"], index=False)
-df.head()
+    size = 512
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+    num_workers = 0
+    batch_size = 16
+    best_threshold = 0.5
+    min_size = 3500
+    # device = torch.device("cuda:0")
+    device = torch.device("mps")
+    # df = pd.read_csv("./results.csv")
+    df = pd.DataFrame(columns=["ImageId", "EncodedPixels"])
+    testset = DataLoader(
+        TestDataset("./train-rle-test-10.csv", df, size, mean, std),
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=True,
+    )
+    model = smp.Unet("resnet34", encoder_weights="imagenet", activation=None)
+    # model = model_trainer.net  # get the model from model_trainer object
+    model.eval()
+    state = torch.load(
+        "./models/segmentation_restnet34_model.pth",
+        map_location=lambda storage, loc: storage,
+    )
+    print(state)
+    model.load_state_dict(state["state_dict"])
+    encoded_pixels = []
+    print("Model loaded")
+
+    for i, batch in enumerate(tqdm(testset)):
+        preds = torch.sigmoid(model(batch.to(device)))
+        preds = (
+            preds.detach().cpu().numpy()[:, 0, :, :]
+        )  # (batch_size, 1, size, size) -> (batch_size, size, size)
+        for probability in preds:
+            if probability.shape != (1024, 1024):
+                probability = cv2.resize(
+                    probability, dsize=(1024, 1024), interpolation=cv2.INTER_LINEAR
+                )
+            predict, num_predict = post_process(probability, best_threshold, min_size)
+            if num_predict == 0:
+                encoded_pixels.append("-1")
+            else:
+                r = run_length_encode(predict)
+                encoded_pixels.append(r)
+    df["EncodedPixels"] = encoded_pixels
+    df.to_csv("submission.csv", columns=["ImageId", "EncodedPixels"], index=False)
+    df.head()
